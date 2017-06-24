84c2a5e98b5aec81c2a5fd1251f922039489b4cd
Damon Aw <daemonsy@gmail.com>
2017-06-24 17:48:18 -0400

    Make full service update available for flash briefing
    - Expose full service update in a separate handler
    - Deal with the nastiness of flash briefings not supporting SSML
    - Prep for API Gateway to hit an endpoint that runs the flash handler

--- tests/status-to-speech-test.js
+++ tests/status-to-speech-test.js
@@ -6,19 +6,19 @@ test('Given a service status, it builds the right speech"', t => {
   t.plan(4);
 
   t.is(statusToSpeech('A', 'GOOD SERVICE'),
-    'Good service on the <say-as interpret-as="spell-out">A</say-as> line'
+    'Good service on the A line. '
   );
 
   t.is(statusToSpeech('N', 'DELAYS'),
-    'The <say-as interpret-as="spell-out">N</say-as> line is experiencing delays'
+    'The N line is experiencing delays. '
   );
 
   t.is(statusToSpeech('Q', 'PLANNED WORK'),
-    'The <say-as interpret-as="spell-out">Q</say-as> line is undergoing planned work'
+    'The Q line is undergoing planned work. '
   );
 
   t.is(statusToSpeech('1', 'SERVICE CHANGE'),
-    'The <say-as interpret-as="spell-out">1</say-as> line is undergoing service change'
+    'The 1 line is undergoing service change. '
   );
 })
 
@@ -26,19 +26,19 @@ test('Given multiple service statuses, it builds gramatically correct speech out
   t.plan(4);
 
   t.is(statusToSpeech(['ACE', '456'], 'GOOD SERVICE'),
-    'Good service on the <say-as interpret-as="spell-out">ACE<break/>456</say-as> lines'
+    'Good service on the A-C-E, 4-5-6 lines. '
   );
 
   t.is(statusToSpeech(['NQR', 'BDFM'], 'DELAYS'),
-    'The <say-as interpret-as="spell-out">NQR<break/>BDFM</say-as> lines are experiencing delays'
+    'The N-Q-R, B-D-F-M lines are experiencing delays. '
   );
 
   t.is(statusToSpeech(['123', '456'], 'PLANNED WORK'),
-    'The <say-as interpret-as="spell-out">123<break/>456</say-as> lines are undergoing planned work'
+    'The 1-2-3, 4-5-6 lines are undergoing planned work. '
   );
 
   t.is(statusToSpeech(['7', 'ACE'], 'SERVICE CHANGE'),
-    'The <say-as interpret-as="spell-out">7<break/>ACE</say-as> lines are undergoing service change'
+    'The 7, A-C-E lines are undergoing service change. '
   );
 });
 
@@ -46,7 +46,7 @@ test('Given an unknown service status, it builds a generic speech output', t => 
   t.plan(1);
 
   t.is(statusToSpeech('F', 'MTA TROLLING US'),
-    'The status of the <say-as interpret-as="spell-out">F</say-as> line is mta trolling us'
+    'The status of the F line is mta trolling us. '
   );
 });
 
@@ -54,6 +54,6 @@ test('Give a status with different casing, it builds the right speech output', t
   t.plan(1);
 
   t.is(statusToSpeech('B', 'delays'),
-    'The <say-as interpret-as="spell-out">B</say-as> line is experiencing delays'
+    'The B line is experiencing delays. '
   );
 });
