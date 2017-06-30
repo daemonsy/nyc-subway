3b57f6a80e12b442bae8c3aa7b1bf55f12a6aa46
Damon Aw <daemonsy@gmail.com>
2017-06-29 21:14:27 -0400

    I am an idiot

--- handlers/store-favorite-line.js
+++ handlers/store-favorite-line.js
@@ -4,11 +4,11 @@ const closestLineMatcher = require('../utilities/closest-line-matcher.js');
 module.exports = function() {
   let heardNameGroup = this.event.request.intent.slots.subwayLineOrGroup.value;
 
-  if(!nameGroup) { this.emit(':tell', "Sorry, I didn't hear a subway line or group I recognized") };
+  if(!heardNameGroup) { this.emit(':tell', "Sorry, I didn't hear a subway line or group I recognized") };
 
   fetchMTAStatus(statuses => {
     let closestLine = closestLineMatcher(statuses, 'nameGroup', heardNameGroup);
 
-    this.emit('tell', `Your favorite line is ${closestLine.nameGroup}`);
+    this.emit(':tell', `Your favorite line is ${closestLine.nameGroup}`);
   });
 }
